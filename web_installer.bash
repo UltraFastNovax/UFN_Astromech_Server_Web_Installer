43461b14a63feeb5b2c5278466fb9966941e2c58
UltraFastNovax <152449920+UltraFastNovax@users.noreply.github.com>
2024-03-08 17:18:15 -0500

    Create web_installer.bash

---
+++ web_installer.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Check OS compatibility
+cat /etc/os-release
+
+# Create installation directory
+mkdir ./installer
+cd ./installer
+
+# Create downloads directory
+mkdir ./downloads
+cd ./downloads
+wget https://download.oracle.com/java/17/archive/jdk-17.0.10_linux-x64_bin.deb
+wget https://raw.githubusercontent.com/UltraFastNovax/UFN_Astromech_Server_Installer/main/r2_unit.service
+mkdir java
+cd ./java
+#download java files
+cd ../../
+
+# Install Java 17.0.10
+dpkg -i ./downloads/jdk-17.0.10_linux-x64_bin.deb
+
+# Set up service
+mkdir /etc/r2_unit
+cp -r ./downloads/java /etc/r2_unit
+echo "java Main.java" >> /etc/r2_unit/java/start.bash
+echo "pkill java" >> /etc/r2_unit/java/stop.bash
+cp ./downloads/r2_unit.service /etc/systemd/system/r2_unit.service
+systemctl enable /etc/systemd/system/r2_unit.service
+cd ../
+rm -r ./installer
+echo "Finished install successfully"
